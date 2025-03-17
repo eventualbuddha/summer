480ace7a8ade59df3932535a415b8f05be171f71
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-03-16 17:15:37 -0700

    fix: format zero cents as "$0" not "+$0"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,11 +56,11 @@
 	});
 
 	function formatTransactionAmount(cents: number): string {
-		return `${cents < 0 ? '' : '+'}${(Math.abs(cents) / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`;
+		return `${cents <= 0 ? '' : '+'}${(Math.abs(cents) / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' })}`;
 	}
 
 	function formatWholeDollarAmount(cents: number): string {
-		return `${cents < 0 ? '' : '+'}${(Math.abs(cents) / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 })}`;
+		return `${cents <= 0 ? '' : '+'}${(Math.abs(cents) / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 })}`;
 	}
 </script>
 
