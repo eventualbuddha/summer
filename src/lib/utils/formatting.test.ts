43115f6e9f2d17c651a37cd2a4357b268d3ea73f
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-03-30 21:59:00 -0700

    test: add tests for some utilities

---
+++ src/lib/utils/formatting.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest';
+import { formatTransactionAmount, formatWholeDollarAmount } from './formatting';
+
+test('formatTransactionAmount', () => {
+	expect(formatTransactionAmount(123456789)).toBe('+$1,234,567.89');
+	expect(formatTransactionAmount(-123456789)).toBe('$1,234,567.89');
+});
+
+test('formatWholeDollarAmount', () => {
+	expect(formatWholeDollarAmount(123456789)).toBe('+$1,234,568');
+	expect(formatWholeDollarAmount(-123456789)).toBe('$1,234,568');
+});
