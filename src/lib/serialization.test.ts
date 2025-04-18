ebe586ecc869dcc1a1bcfd99d0a15e946ec1646a
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-04-18 07:10:49 -0700

    test(db): add a more complex nested value

--- src/lib/serialization.test.ts
+++ src/lib/serialization.test.ts
@@ -12,7 +12,12 @@ test.each([
 	new Date(),
 	new StringRecordId('test:1'),
 	[1, 2, 3],
-	{ a: 8, b: new Date() }
+	{ a: 8, b: new Date() },
+	{
+		a: [{ b: 1, c: [0] }, -12345],
+		id: new StringRecordId('table:bob'),
+		p: { date: new Date(), buf: Buffer.of(1, 2, 3, 4, 5) }
+	}
 ])('%o', (value) => {
 	expect(parseRecord(serializeRecord({ a: value }))).toEqual({ a: value });
 });
