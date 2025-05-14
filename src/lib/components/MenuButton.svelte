6e095edb92fd04f23e0ccaa02ea0c2a17a9fcf0a
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-05-14 07:42:58 -0700

    fix: dark mode support for `MenuButton`

--- src/lib/components/MenuButton.svelte
+++ src/lib/components/MenuButton.svelte
@@ -4,22 +4,22 @@
 
 <div class="flex flex-col items-center justify-center" onfocus={(e) => e.currentTarget.blur()}>
 	<span
-		class="block h-0.5 w-6 rounded-sm bg-black
-                    transition-all duration-300 ease-out {isOpen
+		class="block h-0.5 w-6 rounded-sm bg-black transition-all
+                    duration-300 ease-out dark:bg-white {isOpen
 			? 'translate-y-1 rotate-45'
 			: '-translate-y-0.5'}"
 	>
 	</span>
 	<span
 		class="my-0.5 block h-0.5 w-6 rounded-sm
-                    bg-black transition-all duration-300 ease-out {isOpen
+                    bg-black transition-all duration-300 ease-out dark:bg-white {isOpen
 			? 'opacity-0'
 			: 'opacity-100'}"
 	>
 	</span>
 	<span
-		class="block h-0.5 w-6 rounded-sm bg-black
-                    transition-all duration-300 ease-out {isOpen
+		class="block h-0.5 w-6 rounded-sm bg-black transition-all
+                    duration-300 ease-out dark:bg-white {isOpen
 			? '-translate-y-1 -rotate-45'
 			: 'translate-y-0.5'}"
 	>
