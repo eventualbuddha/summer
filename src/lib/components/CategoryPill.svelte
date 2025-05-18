b0e167aee14013e085b3b9b74389122fa224eb37
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-05-17 17:06:43 -0700

    fix: improve `CategoryPill` a11y

--- src/lib/components/CategoryPill.svelte
+++ src/lib/components/CategoryPill.svelte
@@ -10,16 +10,19 @@
 </script>
 
 {#if style === 'color'}
-	<span class="h-8 p-1 pr-2 {category.color} {className}" title={category.name}></span>
+	<span class="h-8 p-1 pr-2 {category.color} {className}" title={category.name} aria-hidden="true">
+	</span>
 {/if}
 {#if style === 'full'}
 	<span class="h-8 p-1 pr-2 whitespace-nowrap {category.color} {className}" title={category.name}>
-		{category.emoji}
+		<span aria-hidden="true">{category.emoji}</span>
 		{category.name}
 	</span>
 {/if}
 {#if style === 'short'}
 	<span class="h-8 p-1 pr-2 whitespace-nowrap {category.color} {className}" title={category.name}>
-		{category.emoji}
+		<span role="img" aria-label={category.name}>
+			{category.emoji}
+		</span>
 	</span>
 {/if}
